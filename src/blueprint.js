f783a102b19a084b1e1e637c3ad6123789e61e7a
akafred <github@akafred.com>
2011-06-11 22:45:46 +0200

    Implementert rotering.

--- src/blueprint.js
+++ src/blueprint.js
@@ -1,11 +1,60 @@
+/*jslint newcap:false */
 
 var ZOMBIE = this.ZOMBIE || {};
 
-ZOMBIE.blueprint = {
-    create: function create(shape) {
-        if (!shape) throw new TypeError("shape must be array");    
+(function () {
+
+    function validate(shape) {
+        if (!shape) {
+            throw new TypeError("shape must be array");    
+        }
+        if (!shape.length || !shape[0].length) {
+            throw { name: "ArgumentError", message: "shape cannot be empty" };
+        }
+        for (var row = 0; row < shape.length; row++) {
+            if (shape[row].length !== shape[0].length) {
+                throw  { name: "ArgumentError", message: "shape strings must be of equal length" };
+            }
+        }
+    }
+    
+    function create(shape) {
+        validate(shape);
         var self = Object.create(this);
         self.shape = shape;
         return self;
     }
-};
+
+    function width() {
+        return this.shape[0].length;
+    }
+    
+    function columns() {
+        var result = [];
+        for (var col = 0; col < this.width(); col++) {
+            result.push(this.column(col));
+        }
+        return result;
+    }
+
+    function column(col) {
+        var result = "";
+        for (var row = 0; row < this.shape.length; row++) {
+            result = result + this.shape[row][col];
+        }
+        return result;        
+    }
+
+    function rotate90() {
+        this.shape = this.columns().reverse();
+        return this;
+    }
+
+    ZOMBIE.blueprint = {
+        create: create,
+        columns: columns,
+        column: column,
+        width: width,
+        rotate90: rotate90
+    };
+} ());
--- test/blueprint_test.js
+++ test/blueprint_test.js
@@ -1,26 +1,22 @@
-/*jslint newcap:true */
+/*jslint newcap:false */
 /*globals TestCase, ZOMBIE, assertEquals, assertException, assertObject, assertPrototypeOf, sinon */
 
 (function (blueprint) {
     TestCase("BlueprintTest", sinon.testCase({
 
-        setUp: function () {
-            blueprint = ZOMBIE.blueprint;
-        },
-
         "test should be an object": function () {
             assertObject(blueprint);
-        },  
+        },
 
         "test should create blueprints": function () {
-            var bp = blueprint.create([]);
+            var bp = blueprint.create(['*']);
             assertPrototypeOf(blueprint, bp);
         },
 
         "test should have shape": function () {
             var shape = ['***',
                          '***'];
-            var bp = blueprint.create(shape);             
+            var bp = blueprint.create(shape);
             assertEquals(shape, bp.shape);
         },
 
@@ -28,7 +24,63 @@
             assertException(function () {
                 blueprint.create();
             });
-        }
+        },
+        
+        "test should complain about empty shape": function () {
+            assertException(function () {
+                blueprint.create([]);
+            });
+        },
+        
+        "test should complain about irregular shape": function () {
+            assertException(function () {
+                blueprint.create(['*', '']);
+            });
+        },
 
+        "test should return columns": function () {
+            var bp = blueprint.create(['* ',
+                                       '**']);
+            assertEquals(['**', ' *'], bp.columns());            
+        },
+        
+        "test should have return correct width": function () {
+            var bp = blueprint.create(['**']);
+            assertEquals(2, bp.width());
+        }
+        
     }));
+
+    function assertRotation(before, after) {
+        var bp = blueprint.create(before);
+        assertEquals(after, bp.rotate90().shape);
+    }
+
+    TestCase("BlueprintRotateTest", sinon.testCase({
+        "test should rotate single cell": function () {
+            assertRotation(['*'], ['*']);
+        },
+
+        "test should rotate vertical line": function () {
+            assertRotation(['*', '*'], ['**']);
+        },
+
+        "test should rotate horizontal line": function () {
+            assertRotation(['**'], ['*', '*']);
+        },
+        
+        "test should rotate L-shape": function () {
+            assertRotation(["**", "* "], ["* ", "**"]);
+        },
+        
+        "test should rotate T-shape": function () {
+            assertRotation(["***",
+                            " * ",
+                            " * "], 
+                           ["*  ",
+                            "***",
+                            "*  "]);
+        }
+    }));
+
 }(ZOMBIE.blueprint));
