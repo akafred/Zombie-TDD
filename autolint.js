a704b75e89f13c4ba245a72262ed80f82b72f1a7
kjejorge <kjejorge@finn.no>
2012-07-11 18:53:26 +0200

    Removed lib-folder from autolint-configuration.

--- autolint.js
+++ autolint.js
@@ -1,12 +1,11 @@
 /*globals module*/
 
 module.exports = {
-    paths: [ "./**/*.js" ],   // a list of paths to the files you want linted
+    paths: [ "./src/*.js", "./test/*.js" ],   // a list of paths to the files you want linted
     linter: "jslint",         // optionally: jshint
     linterOptions: {          // see default-configuration.js for a list of all options
         "goodparts": true,
         "onevar": false,
         "maxlen": 140
-    },
-    excludes: [ "sinon" ]              // a list of strings/regexes matching filenames that should not be linted
+    }
 };
